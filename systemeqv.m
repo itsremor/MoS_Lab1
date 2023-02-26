96b428ecc96f9caa3aa8d2f17be4770c94e5e5a5
itsremor <akamorganz@gmail.com>
2023-02-26 20:21:13 +0300

    Reworked :)

--- systemeqv.m
+++ systemeqv.m
@@ -1,13 +1,13 @@
-% Относительно примера - a = мю (первая переменная), b = дельта (Вторая
+% Относительно примера - b = мю (первая переменная), c = дельта (Вторая
 % переменная), x = u
 
 %Для меня a = фактор b, b = фактор c
-function u=systemeqv(a,b);
+function u=systemeqv(b,c);
 multip_var = 1;
-for i = 1:b
+for i = 1:c
     multip_var = multip_var * randn;
 end
 
 %датчик логнормального распределения
-u=-a * log(multip_var);
+u=-b * log(multip_var);
 return;
